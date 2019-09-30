ce4233a14b28aa3153e6a3dcea0b3e2e214b7d36
Gurjot <1313gurjotsingh@gmail.com>
2019-09-30 22:42:01 +0530

    js, css refactor

--- bynk_loany/assets/js/loany.js
+++ bynk_loany/assets/js/loany.js
@@ -1,16 +1,16 @@
 // console.error("asdadas")
 
-// function getFormData($form) {
-//     var unindexed_array = $form.serializeArray();
-//     var indexed_array = {};
+function getFormData($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
 
-//     $.map(unindexed_array, function (n, i) {
-//         indexed_array[n['name']] = n['value'];
-//     });
+    $.map(unindexed_array, function (n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
 
-//     final_arr = { "user": indexed_array }
-//     return final_arr;
-// }
+    final_arr = { "user": indexed_array }
+    return final_arr;
+}
 
 // $("form").submit(function(e){
 //     console.log('here')
@@ -43,20 +43,25 @@ $(document).ready(function(){
     onSubmitLoanyForm = () => {
         $('.loanyApplicationForm').hide();
         $('.loanyLoader').show();
+        var form_data = $('form').serialize()
+        console.log(form_data)
+        var $form = $("form");
+        var data = getFormData($form);
         $.ajax({
             type: 'POST',
-            url: "https://reqres.in/api/register", // For 404
+            url: "/api/users", // For 404
             // url: "https://reqres.in/api/user", // For 200
-            data: $('#loanyForm').serialize(),
+            data: data,
             success: function(response) {
+                console.log(response)
                 $('.loanyLoader').hide();
                 $('.loanySuccessFormContent').show();
                 // Filling server Info
-                $('.loanySuccessFormContent .loanySuccessFormContentLoanAmount h3').append(`${response.id}%`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(1)').append(`${response.name}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(2)').append(`${response.email}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(3)').append(`${response.phone}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(4)').append(`${response.requestAmount}`)
+                $('.loanySuccessFormContent .loanySuccessFormContentLoanAmount h3').append(`${response.data.rate_of_interest}%`)
+                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(1)').append(`${response.data.name}`)
+                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(2)').append(`${response.data.email}`)
+                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(3)').append(`${response.data.phone_number}`)
+                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(4)').append(`${response.data.loan_amount}`)
             },
             error: function(errorRes) {
                 const {responseJSON:{error}} = errorRes;
