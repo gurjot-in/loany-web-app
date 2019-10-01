634283ce13d05149d3298d986176130c08d66089
gurjot-in <1313gurjotsingh@gmail.com>
2019-10-01 14:57:18 +0530

    major UI changes and js refactor to fix UI issues

--- bynk_loany/assets/js/loany.js
+++ bynk_loany/assets/js/loany.js
@@ -1,91 +1,77 @@
-// console.error("asdadas")
+$(document).ready(function() {
+  onSubmitLoanyForm = () => {
+    $(".loanyApplicationForm").hide();
+    $(".loanyLoader").show();
+    var form_data = $("form").serialize();
+    var $form = $("form");
+    var data = getFormData($form);
+    $.ajax({
+      type: "POST",
+      url: "/api/users",
+      data: data,
+      success: function(response) {
+        const {
+          is_approved,
+          rate_of_interest,
+          email,
+          phone_number,
+          loan_amount,
+          name
+        } = response.data;
+        if (is_approved) {
+          $(".loanyLoader").hide();
+          $(".loanySuccessFormContent").show();
+          // Filling server Info
+          $(
+            ".loanySuccessFormContent .loanySuccessFormContentLoanAmount h3"
+          ).append(`${rate_of_interest}%`);
+          $(
+            ".loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(1)"
+          ).append(`${name}`);
+          $(
+            ".loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(2)"
+          ).append(`${email}`);
+          $(
+            ".loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(3)"
+          ).append(`${phone_number}`);
+          $(
+            ".loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(4)"
+          ).append(`${loan_amount} sek`);
+        } else {
+          showErrorPage();
+        }
+      },
+      error: function(errorRes) {
+        const { statusText } = errorRes;
+        showErrorPage(statusText);
+      }
+    });
+  };
 
-function getFormData($form) {
+  tryAgain = () => {
+    //Only show from
+    $(".loanyFailureFormContent").hide();
+    $(".loanySuccessFormContent").hide();
+    $(".loanyApplicationForm").show();
+    $("#loanyForm").trigger("reset");
+  };
+
+  showErrorPage = (message = "It seems like we couldn't grant loan") => {
+    $(".loanyLoader").hide();
+    $(".loanyFailureFormContent").show();
+    $(".loanyfailureFormContentLoanAmount h4").html(message);
+    setTimeout(() => tryAgain(), 3000);
+  };
+
+  getFormData = ($form) => {
     var unindexed_array = $form.serializeArray();
     var indexed_array = {};
 
-    $.map(unindexed_array, function (n, i) {
-        indexed_array[n['name']] = n['value'];
+    $.map(unindexed_array, function(n, i) {
+      indexed_array[n["name"]] = n["value"];
     });
 
-    final_arr = { "user": indexed_array }
+    final_arr = { user: indexed_array };
     return final_arr;
-}
-
-// $("form").submit(function(e){
-//     console.log('here')
-//     e.preventDefault();
-//     console.log('clicked')
-//     var form_data = $('form').serialize()
-//     console.log(form_data)
-//     var $form = $("form");
-//     var data = getFormData($form);
-//     console.log(data)
-//     // $.ajax(settings).done(function (response) {
-//     //     console.log(response);
-//     // });
-//     $.ajax({
-//         type: 'POST',
-//         url: '/api/users',
-//         data: data,
-//         dataType: "json",
-//         success: function (data) {
-//             console.error(data)
-//             alert("success!");
-//         },
-//         error: function () {
-//             alert('error!');
-//         }
-//     });
-// });
-
-$(document).ready(function(){
-    onSubmitLoanyForm = () => {
-        $('.loanyApplicationForm').hide();
-        $('.loanyLoader').show();
-        var form_data = $('form').serialize()
-        console.log(form_data)
-        var $form = $("form");
-        var data = getFormData($form);
-        $.ajax({
-            type: 'POST',
-            url: "/api/users", // For 404
-            // url: "https://reqres.in/api/user", // For 200
-            data: data,
-            success: function(response) {
-                console.log(response)
-                loan_status = response.data.status
-                if (loan_status == true){
-                $('.loanyLoader').hide();
-                $('.loanySuccessFormContent').show();
-                // Filling server Info
-                $('.loanySuccessFormContent .loanySuccessFormContentLoanAmount h3').append(`${response.data.rate_of_interest}%`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(1)').append(`${response.data.name}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(2)').append(`${response.data.email}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(3)').append(`${response.data.phone_number}`)
-                $('.loanySuccessFormContent .loanySuccessUserDetailsInfo h4:nth-child(4)').append(`${response.data.loan_amount}`)
-                }
-                else {
-                    $('.loanyLoader').hide();
-                    $(".loanyFailureFormContent").show();
-                    setTimeout(() => tryAgain(),5000);
-                }
-            },
-            error: function(errorRes) {
-                const {responseJSON:{error}} = errorRes;
-                console.log(error);
-                // $('.loanyLoader').hide();
-                // $(".loanyFailureFormContent").show();
-                // setTimeout(() => tryAgain(),3000);
-            }
-        });
-    }
-
-    tryAgain = () => {
-        //ONly show from
-        $('.loanyFailureFormContent').hide();
-        $('.loanySuccessFormContent').hide();
-        $('.loanyApplicationForm').show();
-        $("#loanyForm").trigger('reset');
-    }
+  }
 });
