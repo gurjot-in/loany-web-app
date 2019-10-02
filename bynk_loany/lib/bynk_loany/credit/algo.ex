745568706486af2fbbae1695a2b2e0170b43c31d
gurjot-in <1313gurjotsingh@gmail.com>
2019-10-02 15:49:17 +0530

    removing unused utils

--- bynk_loany/lib/bynk_loany/credit/algo.ex
+++ bynk_loany/lib/bynk_loany/credit/algo.ex
@@ -2,33 +2,6 @@ defmodule BynkLoany.Credit.Algo do
     alias BynkLoany.Credit
     alias BynkLoany.Credit.User
 
-
-    def are_conditions_satisfied(attrs) do
-
-      current_changeset = User.changeset(%User{}, attrs)
-      IO.inspect "current changeset"
-      IO.inspect current_changeset
-  
-      with true <- current_changeset.valid? do
-        {:ok}
-      else
-        err -> err
-        IO.inspect err
-        {:error, current_changeset.errors}    
-      end  
-  
-    end
-    
-
-    def test() do
-        # {:ok, 999}
-        Cachex.put(:my_cache, "key", %{"one" => :two, 3 => "four", tt: 5})
-        {status, result} = Cachex.get(:my_cache, "key")
-        IO.inspect result[:tt]
-        IO.inspect "checking prime"
-        IO.inspect prime?(47)
-    end
-
     def find_min_from_db(amount) do
         IO.inspect "here in db"
         users = Credit.list_users()
